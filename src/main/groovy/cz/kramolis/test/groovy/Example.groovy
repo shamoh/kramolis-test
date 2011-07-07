175e9c11f7e73f0d81e7c72fa420199f9fd83371
Libor Kramolis <libor.kramolis@gmail.com>
2011-07-07 22:16:56 +0200

    Added grooovy dependencies.

---
+++ src/main/groovy/cz/kramolis/test/groovy/Example.groovy
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package cz.kramolis.test.groovy
+
+/**
+ * Example Groovy class.
+ */
+class Example
+{
+    def show() {
+        println 'Hello World'
+    }
+}
---
+++ src/main/groovy/cz/kramolis/test/groovy/Helper.java
@@ -0,0 +1,15 @@
+//
+// Generated from archetype; please customize.
+//
+
+package cz.kramolis.test.groovy;
+
+/**
+ * Example Java class.
+ */
+public class Helper
+{
+    public void help(final Example example) {
+        example.show();
+    }
+}
